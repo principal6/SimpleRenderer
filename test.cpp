02075e7f298dc38bdd319b573777e726e0157f28
Jangwon Jesus Kim <jesuskim666@gmail.com>
2022-08-31 00:35:24 +0900

    implemented (alpha) Texture rendering

--- test.cpp
+++ test.cpp
@@ -6,29 +6,32 @@ R"(
     {
         float4 position : POSITION0;
         float4 color : COLOR0;
+        float4 texcoord : TEXCOORD0;
     };
     struct VS_OUTPUT
     {
         float4 screenPosition : SV_POSITION;
         float4 color : COLOR0;
+        float4 texcoord : TEXCOORD0;
     };
 )";
 
 const char kVertexShaderCode0[] =
 R"(
     #include "StreamData"
-
+    
     cbuffer CB_MATRICES
     {
         float4x4 _cbProjectionMatrix;
     };
-
+    
     VS_OUTPUT main(VS_INPUT input)
     {
         VS_OUTPUT output;
         output.screenPosition = mul(input.position, _cbProjectionMatrix);
         output.screenPosition /= output.screenPosition.w;
         output.color = input.color;
+        output.texcoord = input.texcoord;
         return output;
     }
 )";
@@ -36,10 +39,24 @@ R"(
 const char kPixelShaderCode0[] =
 R"(
     #include "StreamData"
-
+    
     float4 main(VS_OUTPUT input) : SV_Target
     {
         return input.color;
+    }
+)";
+
+const char kPixelShaderCode_Font[] =
+R"(
+    #include "StreamData"
+    
+    sampler g_sampler0;
+    Texture2D<float4> g_texture0;
+    float4 main(VS_OUTPUT input) : SV_Target
+    {
+        float4 sampled = g_texture0.Sample(g_sampler0, input.texcoord.xy);
+        float4 sampledSqrt = sqrt(sampled.r);
+        return float4(sampledSqrt);
     }
 )";
 
@@ -51,6 +68,7 @@ int main()
     {
         float4 _position;
         float4 _color;
+        float2 _texcoord;
     };
     
     struct CB_MATRICES
@@ -73,11 +91,14 @@ int main()
     ShaderInputLayout shaderInputLayout;
     shaderInputLayout.pushInputElement(ShaderInputLayout::createInputElementFloat4("POSITION", 0));
     shaderInputLayout.pushInputElement(ShaderInputLayout::createInputElementFloat4("COLOR", 0));
+    shaderInputLayout.pushInputElement(ShaderInputLayout::createInputElementFloat2("TEXCOORD", 0));
     shaderInputLayout.create(renderer, vertexShader0);
     renderer.bindShaderInputLayout(shaderInputLayout);
 
     Shader pixelShader0;
     pixelShader0.create(renderer, kPixelShaderCode0, ShaderType::PixelShader, "PixelShader0", "main", "ps_5_0", &shaderHeaderStreamData);
+    Shader fontPixelShader;
+    fontPixelShader.create(renderer, kPixelShaderCode_Font, ShaderType::PixelShader, "FontPixelShader", "main", "ps_5_0", &shaderHeaderStreamData);
     renderer.bindShader(pixelShader0);
 
     Resource vscbMatrices;
@@ -88,6 +109,17 @@ int main()
     }
     renderer.bindShaderResource(ShaderType::VertexShader, vscbMatrices, 0);
 
+    Resource fontTexture;
+    byte bytes[kFontTextureByteCount];
+    for (uint32 iter = 0; iter < kFontTextureByteCount; iter++)
+    {
+        const uint32 bitAt = iter % 8;
+        const uint32 byteAt = iter / 8;
+        const byte byte_ = (kFontTextureRawBitData[byteAt] >> (7 - bitAt)) & 1;
+        bytes[iter] = byte_ * 255;
+    }
+    fontTexture.createTexture2D(renderer, TextureFormat::R8_UNORM, bytes, 128, 60);
+    
     Resource vertexBuffer;
     Resource indexBuffer;
     std::vector<VS_INPUT> vertices;
@@ -105,14 +137,36 @@ int main()
         vertexBuffer.createBuffer(renderer, ResourceType::VertexBuffer, &vertices[0], sizeof(VS_INPUT), (uint32)vertices.size());
         indexBuffer.createBuffer(renderer, ResourceType::IndexBuffer, &indices[0], sizeof(uint32), (uint32)indices.size());
     }
-    renderer.bindInput(vertexBuffer, 0);
-    renderer.bindInput(indexBuffer, 0);
+    
+    Resource fontVertexBuffer;
+    Resource fontIndexBuffer;
+    std::vector<VS_INPUT> fontVertices;
+    std::vector<uint32> fontIndices;
+    {
+        MeshGenerator<VS_INPUT>::push_2D_rectangle(float2(128, 240), float2(256, 480), 0.0f, fontVertices, fontIndices);
+        fontVertices[0]._texcoord = float2(0, 0);
+        fontVertices[1]._texcoord = float2(1, 0);
+        fontVertices[2]._texcoord = float2(0, 1);
+        fontVertices[3]._texcoord = float2(1, 1);
+        fontVertexBuffer.createBuffer(renderer, ResourceType::VertexBuffer, &fontVertices[0], sizeof(VS_INPUT), (uint32)fontVertices.size());
+        fontIndexBuffer.createBuffer(renderer, ResourceType::IndexBuffer, &fontIndices[0], sizeof(uint32), (uint32)fontIndices.size());
+    }
 
     while (renderer.isRunning())
     {
         if (renderer.beginRendering())
         {
-            renderer.drawIndexed((uint32)indices.size());
+            //renderer.bindShader(pixelShader0);
+            //renderer.bindInput(vertexBuffer, 0);
+            //renderer.bindInput(indexBuffer, 0);
+            //renderer.drawIndexed((uint32)indices.size());
+
+            renderer.bindShader(fontPixelShader);
+            renderer.bindInput(fontVertexBuffer, 0);
+            renderer.bindInput(fontIndexBuffer, 0);
+            renderer.bindShaderResource(ShaderType::PixelShader, fontTexture, 0);
+            renderer.drawIndexed((uint32)fontIndices.size());
+
             renderer.endRendering();
         }
     }
